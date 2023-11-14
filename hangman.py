39f27d6364c4cefbc3b1e0fa4adb3e9f047865a5
Navan <navanh89@yahoo.com>
2023-11-14 17:41:08 -0500

    added gui

--- hangman.py
+++ hangman.py
@@ -1,54 +1,94 @@
-import random
+import requests, random
+from tkinter import *
 
-words = []
-for line in open('poolOfWords.csv'):  # creates a list of over 1300 words to use
-    line = line.rstrip('\n')
-    line = line.split(',')
-    if line[0] == '':
-        break
-    for word in line:
-        words.append(word)
+url =  'https://www.randomlists.com/data/vocabulary-words.json'
+r = requests.get(url)
+allWords = r.json()['data']
+word = (random.choice(allWords)['name']).upper()
+characters = list(word)
+placeholder = ['_'] * len(word);
+parts = ["O", "\n/", "|", "\\", "\n/ ",  "\\"]
+guesses = 6;
+alreadyGuessedList = []
+wrongLettersList = []
 
-del words[0]
 
-word = words[random.randint(0, len(words) - 1)].upper()
-placeholder = []
-for i in range(len(word)):
-    placeholder.append('_')
+window = Tk()
+window.geometry('1280x720')
+window.title("Hangman")
+window['bg'] = '#e5e9f0'
+title = Label(window, text="Hangman", font = 'Comfortaa', bg = '#e5e9f0', fg = '#2e3440')
+title.place(relx=0.5, rely=0.0, anchor = 'n')
 
-guessesLeft = len(word) // 2 + len(word) // 3 + 5
+man = StringVar()
+man.set("")
+wrongLetters = StringVar()
+wrongLetters.set("Wrong Letters: " + " ".join(wrongLettersList))
+placeholderText = StringVar()
+placeholderText.set(" ".join(placeholder))
+entry = Entry(window, width= 20, bd = 5, font = 'Comfortaa', fg ='#2e3440')
+errormsg = StringVar()
+errormsg.set("")
+answer = StringVar()
+answer.set("")
 
-temp = list(word)  # extracts each correct letter from the word
-wrongLetters = []
-alreadyGuessed = []  # keeps track of letters that have already been inputted
 
-while guessesLeft > 0:
-    print(placeholder)
-    print('Number of guesses left:', guessesLeft)
-    print('Wrong Letters: ', wrongLetters)
-    print()
-    answer = input('Guess a letter.\n').upper()
-    if answer == word:  # the user inputs the correct word
-        break
-    elif len(answer) > 1:  # the user inputs multiple characters (excluding the correct word)
-        print('Enter a single character!')
-    elif answer in alreadyGuessed:  # the user inputs a letter that they put in before
-        print('You already guessed that letter.')
-    elif answer in word:  # the user inpurs a correct letter
-        while answer in temp:
-            replace = temp.index(answer)
-            placeholder[replace] = answer
-            temp[replace] = '_'
-    else:  # the user inputs an incorrect character
-        print('Nope!\n')
-        alreadyGuessed.append(answer)
-        wrongLetters.append(answer)
-        guessesLeft -= 1
-    if '_' not in placeholder:  # all correct letters have been guessed
-        break
+def close():
+    window.destroy();
 
-if guessesLeft > 0:  # the while loop breaks without using all available guesses
-    print('Correct!, the word was', word)
+def placeChar():
+    errormsg.set("")
+    global guesses
+    if guesses == 0:
+        return
+    c = entry.get().upper()
+    entry.select_clear()
+    if c == word:
+        answer.set("Correct! The word is " + word)
+        guesses == 0
+        button.configure(command = close, text = "Close")
+    elif len(c) > 1:
+        errormsg.set("Please enter one character.")
+    elif c in placeholder or c in wrongLettersList:
+        errormsg.set("You already guessed that letter.")
+    elif c in characters:
+        while c in characters:
+            index = characters.index(c)
+            placeholder[index] = c
+            characters[index] = ''
+            placeholderText.set(' '.join(placeholder))
+        if '_' not in placeholder:
+            answer.set("Correct! The word is " + word)
+            button.configure(command = close, text = "Close")
+        entry.delete(0, "end")
+    else:
+        wrongLettersList.append(c)
+        guesses -= 1
+        man.set(''.join(parts[:(6-guesses)]))
+        wrongLetters.set("Wrong Letters: " + " ".join(wrongLettersList))
+        if guesses == 0:
+            answer.set("The word we were looking for was " + word)
+            button.configure(command = close, text = "Close")
+        entry.delete(0, "end")
 
-else:
-    print('The word we were looking for was', word + '!')
+
+button = Button(window,text="Enter", font='Comfortaa', fg = '#2e3440', bg = '#d8dee9', command = placeChar, width = 20)
+button.place(relx = 0.5, rely = 0.8, anchor = 'n')
+
+Label(window, textvariable=man, font = 'Comfortaa', bg = '#e5e9f0', fg = '#2e3440').place(relx=0.5, rely=0.2, anchor = 'n')
+Label(window, textvariable=placeholderText, font = 'Comfortaa', bg = '#e5e9f0', fg = '#2e3440').place(relx=0.5, rely=0.4, anchor = 'n')
+Label(window, textvariable=wrongLetters, font = 'Comfortaa', bg = '#e5e9f0', fg = '#2e3440').place(relx=0.5, rely=0.5, anchor = 'n')
+Label(window, textvariable=answer, font = 'Comfortaa', bg = '#e5e9f0', fg = '#2e3440').place(relx=0.5, rely=0.6, anchor = 'n')
+Label(window, textvariable=errormsg, font = 'Comfortaa', bg = '#e5e9f0', fg = '#2e3440').place(relx=0.5, rely=0.9, anchor = 'n')
+entry.place(relx = 0.5, rely = 0.7, anchor = 'n')
+
+window.mainloop()
+
+
+
+
+    
+    
+    
+
+
